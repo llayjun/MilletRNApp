4f99d33a277ab84c8257f33e88a2944918f84ba8
llayjun <1218543596@qq.com>
2020-12-08 11:00:07 +0800

    net util

--- src/page/work/work.js
+++ src/page/work/work.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Button,
+    Alert
   } from 'react-native';
+
+import NetUtil from '../../net/net_util';
+
 
 class WorkPage extends Component {
 
-    render() {
+  // class中使用state
+  state = {message: ""};
 
-        return (
-            <View style={styles.container}>
-               <Text>WorkPage</Text>
-            </View>
-        );
-    }
+  get(callback) {
+    NetUtil.get('https://api.apishop.net/common/air/getCityPM25Detail', function(ret) {
+      // Alert.alert('提示', ret['desc']);
+      callback(ret['desc']);
+    });
+  }
+
+  render() {
+
+      return (
+          <View style={styles.container}>
+              <Text>{this.state.message == ""? "WorkPage": this.state.message}</Text>
+              <Button title={'点击请求'} onPress={() => {
+                this.get((desc) => {
+                  this.setState({message: desc});
+                });
+              }}></Button>
+          </View>
+      );
+  }
 
 }
 
